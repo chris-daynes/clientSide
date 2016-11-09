dcf13d71cc31b403464db9e7bd7f06536a776b4c
kfrn <knfrances@gmail.com>
2016-11-10 11:53:19 +1300

    Removed all react stuff

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from './components/app'
-
-render(<App name='election_tweets' />, document.querySelector('main'))
+ var main = document.querySelector('main')
 console.log('welcome to election_tweets')
