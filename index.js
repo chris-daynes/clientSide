222c1d3bd0064bf521a726f01c3cbcef9cbddaa7
kfrn <knfrances@gmail.com>
2016-11-10 16:07:40 +1300

    Fixing images

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ console.log('welcome to election_tweets')
 var don = h('div', {id: "don"},
 
   h('h1.r', 'Donald Trump'),
-  h('img', {src: '../images/trump.png'})
+  h('img', {src: 'images/trump.png'})
 
 )
 
 var hil = h('div', {id: "hil"},
 
   h('h1.d', 'Hillary Clinton'),
-  h('img', {src: '../images/hilary.png'})
+  h('img', {src: 'images/hilary.png'})
 
 )
 
