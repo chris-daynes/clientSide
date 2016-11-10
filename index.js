15c34bd452400a7c30df41f6e3f56e1555b8732a
kfrn <knfrances@gmail.com>
2016-11-10 15:27:04 +1300

    Updates

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+var h = require('hyperscript')
+
  var main = document.querySelector('main')
 console.log('welcome to election_tweets')
+
+var don = h('div', {id: "don"},
+
+  h('h1.r', 'Donald Trump'),
+  h('img', {src: '../images/trump.png'})
+
+)
+
+var hil = h('div', {id: "hil"},
+
+  h('h1.d', 'Hillary Clinton'),
+  h('img', {src: '../images/hilary.png'})
+
+)
+
+
+main.appendChild(don)
+main.appendChild(hil)
