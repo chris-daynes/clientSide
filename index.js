51b80f352e8be03c992ca81ff7717de52095c865
kfrn <knfrances@gmail.com>
2016-11-11 13:57:18 +1300

    Updates

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var h = require('hyperscript')
+var request = require('superagent')
+var {getTweets} = require('./getTweets')
+var endpoint = 'https://election-tweets-dev.herokuapp.com/api/v1/tweets'
 
  var main = document.querySelector('main')
 console.log('welcome to election_tweets')
@@ -6,17 +9,18 @@ console.log('welcome to election_tweets')
 var don = h('div', {id: "don"},
 
   h('h1.r', 'Donald Trump'),
+  getTweets(),
   h('img', {src: 'images/trump.png'})
 
 )
 
-var hil = h('div', {id: "hil"},
-
+var hil = h('div', {id: "hil"}, [
   h('h1.d', 'Hillary Clinton'),
-  h('img', {src: 'images/hilary.png'})
-
+  getTweets(),
+  h('img', {src: 'images/hilary.png'}),
+  // h('button', {onclick: getTweets}, 'Click for tweet')
+]
 )
-
 
 main.appendChild(don)
 main.appendChild(hil)
